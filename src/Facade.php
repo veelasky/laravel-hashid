3c586c1ee492ab962b1170640b9cb71fbfeffccd
Rifki Alhuraibi <rifki.alhuraibi@yandex.com>
2023-08-19 20:16:44 +0700

    (chore) sytleci fix

--- src/Facade.php
+++ src/Facade.php
@@ -3,13 +3,13 @@
 namespace Veelasky\LaravelHashId;
 
 /**
- * @method static \Hashids\Hashids[]|array all()
- * @method static int|null hashToId(string $hash, string $key = 'default')
- * @method static string idToHash(int $id, string $key = 'default')
- * @method static \Hashids\Hashids make(string $key, string $salt)
+ * @method static \Hashids\Hashids[]|array           all()
+ * @method static int|null                           hashToId(string $hash, string $key = 'default')
+ * @method static string                             idToHash(int $id, string $key = 'default')
+ * @method static \Hashids\Hashids                   make(string $key, string $salt)
  * @method static \Veelasky\LaravelHashId\Repository set(string $key, \Hashids\Hashids $hashids)
- * @method static \Hashids\Hashids get(string $key)
- * @method static bool has(string $key)
+ * @method static \Hashids\Hashids                   get(string $key)
+ * @method static bool                               has(string $key)
  */
 class Facade extends \Illuminate\Support\Facades\Facade
 {
